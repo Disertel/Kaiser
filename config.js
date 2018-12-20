25afb60d209a659eff5e0d078b164390a2513abd
Disertel <45638607+Disertel@users.noreply.github.com>
2018-12-20 19:07:41 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'kaiser-app',
+		wp_ws_url : 'http://kaiserpro.es/wp-appkit-api/kaiser-app',
+		wp_url : 'http://kaiserpro.es',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : '',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'F37#HjZ)yC3U@M6Jkf~2jB}./;}|NeCs_TL`]3BSHeh&yAf}m:{J5rZ@s#~MRsY@',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
